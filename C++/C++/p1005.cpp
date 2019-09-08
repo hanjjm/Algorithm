4c8d4ca637bcf16752a118c29578a1e3f3e16343
hanjjm <hanjjm1994@naver.com>
2019-09-09 03:02:08 +0900

    C++ : p1004

---
+++ C++/C++/p1005.cpp
@@ -0,0 +1,14 @@
+//
+//  p1005.cpp
+//  C++
+//
+//  Created by KimHanJu on 09/09/2019.
+//  Copyright © 2019 KimHanJu. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+}
