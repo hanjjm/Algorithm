211226018982c27862e2ca42e2947cef2740d1ae
hanjjm <hanjjm1994@naver.com>
2019-09-16 02:57:55 +0900

    Java : p10718 (입출력과 사칙연산)

---
+++ Java/src/main/java/p10718.java
@@ -0,0 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class p10718 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("강한친구 대한육군\n" +
+				"강한친구 대한육군");
+		bw.flush();
+		bw.close();
+	}
+}
