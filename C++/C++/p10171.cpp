776c6807c50862d1af4bd1c1dfc5607f15da428e
hanju Kim <hanjjm1994@naver.com>
2019-11-14 22:46:06 +0900

    C++ : p10171 (입출력)

---
+++ C++/C++/p10171.cpp
@@ -0,0 +1,16 @@
+//
+//  p10171.cpp
+//  C++
+//
+//  Created by KimHanJu on 2019/11/14.
+//  Copyright © 2019 KimHanJu. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\\    /\\\n )  ( ')\n(  /  )\n \\(__)|";
+    return 0;
+}
