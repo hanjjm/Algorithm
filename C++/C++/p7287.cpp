25d3a25b3828f19f459111cb40dea255e822beed
hanju Kim <hanjjm1994@naver.com>
2019-11-14 23:10:23 +0900

    C++ : p7287 (입출력)

---
+++ C++/C++/p7287.cpp
@@ -0,0 +1,15 @@
+//
+//  p7287.cpp
+//  C++
+//
+//  Created by KimHanJu on 2019/11/14.
+//  Copyright © 2019 KimHanJu. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "48" << endl << "hanjjm94";
+    return 0;
+}
