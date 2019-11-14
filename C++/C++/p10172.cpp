97afe390a2a8deb2b6dc653e166cb5539a1fd5ab
hanju Kim <hanjjm1994@naver.com>
2019-11-14 23:07:40 +0900

    C++ : p10172 (입출력)

---
+++ C++/C++/p10172.cpp
@@ -0,0 +1,19 @@
+//
+//  p10172.cpp
+//  C++
+//
+//  Created by KimHanJu on 2019/11/14.
+//  Copyright © 2019 KimHanJu. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+    return 0;
+}
