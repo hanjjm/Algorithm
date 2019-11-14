27d33daf33dcdc6915231a6a4b864c818583c213
hanju Kim <hanjjm1994@naver.com>
2019-11-14 22:51:28 +0900

    C++ : p10869 (사칙연산)

---
+++ C++/C++/p10869.cpp
@@ -0,0 +1,22 @@
+//
+//  p10869.cpp
+//  C++
+//
+//  Created by KimHanJu on 2019/11/14.
+//  Copyright © 2019 KimHanJu. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << endl;
+    cout << A - B << endl;
+    cout << A * B << endl;
+    cout << A / B << endl;
+    cout << A % B << endl;
+    return 0;
+}
