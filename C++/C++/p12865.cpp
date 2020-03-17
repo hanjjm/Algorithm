4633bf497bafdc75e37b492ced5db14d797a756a
hanju Kim <hanjjm1994@naver.com>
2020-03-18 01:08:57 +0900

    C++ : p12865 (Dynamic Programming)

--- C++/C++/p12865.cpp
+++ C++/C++/p12865.cpp
@@ -6,20 +6,11 @@
 //  Copyright © 2020 KimHanJu. All rights reserved.
 //
 
-// 다른문제 풀고 넘어오자!
+// 아직 헷갈림!
 
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-int DP[100000];
-int W[100];
-int V[100];
-
-void init(int w, int v, int n) {
-    W[n] = w;
-    V[n] = v;
-}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -28,24 +19,20 @@ int main() {
     int N, K; //물품의 수, 버틸 수 있는 무게
     cin >> N >> K;
     
-    for(int i = 0; i < 100; i++) {
-        W[i] = 0;
-        V[i] = 0;
-    }
+    int DP[K + 1];
+    pair<int, int> item[N];
     
     for(int i = 0; i < N; i++) {
-        int W, V; //무게, 가치
-        cin >> W >> V;
-        init(W, V, i);
+        cin >> item[i].first >> item[i].second;
     }
     
-    DP[0] = 0;
-    for(int i = 1; i <= K; i++) {
-        for(int j = 1; j <= N; j++) {
-            if(W[j] <= i) DP[i] = max(DP[i], DP[i - W[j]] + V[j]);
+    for(int i = 0; i < K + 1; i++) DP[i] = 0;
+    for(int i = 0; i < N; i++) {
+        for(int j = K; j >= 1; j--) {
+            if(item[i].first <= j) DP[j] = max(DP[j], DP[j - item[i].first] + item[i].second);
         }
     }
     
     if(K == 0) cout << "0" << endl;
-    else cout << DP[K - 1] << endl;
+    else cout << DP[K] << endl;
 }
