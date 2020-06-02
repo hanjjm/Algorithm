8e8ee8958e5541468dbee7b2ef812921c4456711
hanju Kim <hanjjm1994@naver.com>
2020-06-03 01:57:14 +0900

    C++ : 무선 충전 (Simulation & Brute Force)

--- C++/C++/s5644.cpp
+++ C++/C++/s5644.cpp
@@ -1,114 +1,138 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int matrix[12][12] = {0, };
-vector<int> a, b, x, y, C, P;
+int timer, batteryCount;
+vector<pair<int, int>> firstMan, secondMan;
+vector<vector<int>> battery(9);
+int answer;
 
-void walk(int time) {
-    int direction;
-    for(int i = 0; i < time; i++) {
-        cin >> direction;
-        a.push_back(direction);
-    }
-    for(int i = 0; i < time; i++) {
-        cin >> direction;
-        b.push_back(direction);
+void walkFirstMan() {
+    firstMan.clear();
+    pair<int, int> firstStep = make_pair(1, 1), nextStep;
+    firstMan.push_back(firstStep);
+    int step;
+    nextStep = firstStep;
+    for(int i = 0; i < timer; i++) {
+        cin >> step;
+        if(step == 0) nextStep = nextStep;
+        else if(step == 1) nextStep = make_pair(nextStep.first - 1, nextStep.second);
+        else if(step == 2) nextStep = make_pair(nextStep.first, nextStep.second + 1);
+        else if(step == 3) nextStep = make_pair(nextStep.first + 1, nextStep.second);
+        else if(step == 4) nextStep = make_pair(nextStep.first, nextStep.second - 1);
+        firstMan.push_back(nextStep);
     }
 }
 
-pair<int, int> nextAStep(pair<int, int> nowCoor, int time) {
-    pair<int, int> step;
-    if(a[time] == 0) {
-        step = nowCoor;
-    } else if(a[time] == 1) {
-        step.first = nowCoor.first - 1;
-        step.first = nowCoor.second;
-    } else if(a[time] == 2) {
-        step.first = nowCoor.first;
-        step.first = nowCoor.second + 1;
-    } else if(a[time] == 3) {
-        step.first = nowCoor.first + 1;
-        step.first = nowCoor.second;
-    } else if(a[time] == 4) {
-        step.first = nowCoor.first;
-        step.first = nowCoor.second - 1;
+void walkSecondMan() {
+    secondMan.clear();
+    pair<int, int> firstStep = make_pair(10, 10), nextStep;
+    secondMan.push_back(firstStep);
+    int step;
+    nextStep = firstStep;
+    for(int i = 0; i < timer; i++) {
+        cin >> step;
+        if(step == 0) nextStep = nextStep;
+        else if(step == 1) nextStep = make_pair(nextStep.first - 1, nextStep.second);
+        else if(step == 2) nextStep = make_pair(nextStep.first, nextStep.second + 1);
+        else if(step == 3) nextStep = make_pair(nextStep.first + 1, nextStep.second);
+        else if(step == 4) nextStep = make_pair(nextStep.first, nextStep.second - 1);
+        secondMan.push_back(nextStep);
     }
-    return step;
 }
 
-pair<int, int> nextBStep(pair<int, int> nowCoor, int time) {
-    pair<int, int> step;
-    if(b[time] == 0) {
-        step = nowCoor;
-    } else if(b[time] == 1) {
-        step.first = nowCoor.first - 1;
-        step.first = nowCoor.second;
-    } else if(b[time] == 2) {
-        step.first = nowCoor.first;
-        step.first = nowCoor.second + 1;
-    } else if(b[time] == 3) {
-        step.first = nowCoor.first + 1;
-        step.first = nowCoor.second;
-    } else if(b[time] == 4) {
-        step.first = nowCoor.first;
-        step.first = nowCoor.second - 1;
+void inputBattery() {
+    for(int i = 1; i <= batteryCount; i++) battery[i].clear();
+    for(int i = 1; i <= batteryCount; i++) {
+        int dx, dy, range, power;
+        cin >> dx >> dy >> range >> power;
+        battery[i].push_back(dy);
+        battery[i].push_back(dx);
+        battery[i].push_back(range);
+        battery[i].push_back(power);
     }
-    return step;
-}
-void inner() {
-    
 }
 
-void solve(int time, int number) {
-    int aMax = 0, bMax = 0, aTemp = 0, bTemp = 0;
-//    cout << "@@@" << endl;
-//    for(auto item : x) cout << item << " ";
-//    cout << endl << time << endl;
-    pair<int, int> a_next = make_pair(1, 1), b_next = make_pair(10, 10);
-    for(int i = 0; i < time; i++) {
-        cout << "x : " << a_next.first << ", y : " << a_next.second << endl;
-        aTemp = 0; bTemp = 0;
-        for(int j = 0; j < number; j++) {
-//            cout << abs(a_next.first - x[j]) + abs(a_next.second - y[j]) << " < " << C[j] << endl;
-            if(abs(a_next.first - a[j]) + abs(a_next.second - a[j]) <= C[j]) {
-                aTemp = max(aTemp, P[j]);
-            }
-            if(abs(b_next.first - b[j]) + abs(b_next.second - b[j]) <= C[j]) {
-                bTemp = max(bTemp, P[j]);
-            }
-            
+void printStep() {
+    cout << endl << "A의 이동 : ";
+    for(int i = 0; i < timer; i++) cout << "(" << firstMan[i].first << ", " << firstMan[i].second << ") ";
+    cout << endl << "B의 이동 : ";
+    for(int i = 0; i < timer; i++) cout << "(" << secondMan[i].first << ", " << secondMan[i].second << ") ";
+}
+
+void solveOne() {
+    vector<int> batteryInA;
+    vector<int> batteryInB;
+    for(int i = 0; i <= timer; i++) {
+        batteryInA.clear(); batteryInB.clear();
+        if(abs(firstMan[i].first - battery[1][0]) + abs(firstMan[i].second - battery[1][1]) <= battery[1][2]) batteryInA.push_back(1);
+        if(abs(secondMan[i].first - battery[1][0]) + abs(secondMan[i].second - battery[1][1]) <= battery[1][2]) batteryInB.push_back(1);
+        if(batteryInA.size() == 0 && batteryInB.size() == 0) continue;
+        answer += battery[1][3];
+        
+    }
+}
+
+void solve() {
+    vector<int> batteryInA;
+    vector<int> batteryInB;
+    for(int i = 0; i <= timer; i++) {
+        int energyA = 0, energyB = 0;
+        batteryInA.clear(); batteryInB.clear();
+        for(int j = 1; j <= batteryCount; j++) {
+            if(abs(firstMan[i].first - battery[j][0]) + abs(firstMan[i].second - battery[j][1]) <= battery[j][2]) batteryInA.push_back(j);
+            if(abs(secondMan[i].first - battery[j][0]) + abs(secondMan[i].second - battery[j][1]) <= battery[j][2]) batteryInB.push_back(j);
         }
-        aMax += aTemp;
-        bMax += bTemp;
-        a_next = nextAStep(a_next, i);
-        b_next = nextBStep(b_next, i);
+        
+        if(batteryInA.size() == 0) {
+            if(batteryInB.size() == 1) energyB += battery[batteryInB[0]][3];
+            else if(batteryInB.size() >= 2) {
+                for(int j = 0; j < batteryInB.size(); j++) {
+                    if(battery[batteryInB[j]][3] > energyB) energyB = battery[batteryInB[j]][3];
+                }
+            }
+            answer += energyB;
+        }
+        
+        if(batteryInB.size() == 0) {
+            if(batteryInA.size() == 1) energyA += battery[batteryInA[0]][3];
+            else if(batteryInA.size() >= 2) {
+                for(int j = 0; j < batteryInA.size(); j++) {
+                    if(battery[batteryInA[j]][3] > energyA) energyA = battery[batteryInA[j]][3];
+                }
+            }
+            answer += energyA;
+        }
+        
+        int compareNum = 0;
+        if(batteryInA.size() >= 1 && batteryInB.size() >= 1) {
+            for(int j = 0; j < batteryInA.size(); j++) {
+                for(int k = 0; k < batteryInB.size(); k++) {
+                    energyA = battery[batteryInA[j]][3];
+                    energyB = battery[batteryInB[k]][3];
+                    if(batteryInA[j] == batteryInB[k]) compareNum = max(compareNum, energyA);
+                    else compareNum = max(compareNum, energyA + energyB);
+                }
+            }
+            answer += compareNum;
+        }
     }
-//    cout << "aMax : " << aMax << ", bMax : " << bMax << endl;
-    cout << aMax + bMax << endl;
 }
 
-void battery(int number, int time) {
-    int _y, _x, _C, _P;
-    for(int i = 0; i < number; i++) {
-        cin >> _y >> _x >> _C >> _P;
-        x.push_back(_y); y.push_back(_x);
-        C.push_back(_C); P.push_back(_P);
-    }
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int testCase;
+    cin >> testCase;
     
-    solve(time, number);
-}
-
-int main(int argc, char** argv)
-{
-    int test_case;
-    int T;
-    cin>>T;
-    
-    for(test_case = 1; test_case <= T; ++test_case) {
-        int M, A;
-        cin >> M >> A;
-        walk(M);
-        battery(A, M);
+    for(int i = 1; i <= testCase; i++) {
+        cin >> timer >> batteryCount;
+        walkFirstMan();
+        walkSecondMan();
+//        printStep();
+        inputBattery();
+        answer = 0;
+        if(batteryCount == 1) solveOne();
+        else solve();
+        cout << "#" << i << " " <<  answer << endl;
     }
-    return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
