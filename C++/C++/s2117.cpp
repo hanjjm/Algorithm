46b71d603571a52146687b410109106b4a623778
hanju Kim <hanjjm1994@naver.com>
2020-06-03 04:10:43 +0900

    C++ : 홈 방범 서비스 (Simulation & Brute Force)

--- C++/C++/s2117.cpp
+++ C++/C++/s2117.cpp
@@ -1,72 +1,41 @@
-//
-//  s2117.cpp
-//  C++
-//
-//  Created by KimHanJu on 2020/05/28.
-//  Copyright © 2020 KimHanJu. All rights reserved.
-//
-
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <cmath>
 using namespace std;
-int matrix[22][22];
-int visited[22][22];
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+vector<pair<int, int>> house;
+int matrix[20][20];
 int N, M, K;
+int answer;
 
 void initMatrix() {
-    for(int i = 0; i < 22; i++) {
-        for(int j = 0; j < 22; j++) matrix[i][j] = 0;
+    for(int i = 0; i < 20; i++) {
+        for(int j = 0; j < 20; j++) matrix[i][j] = 0;
     }
-    
-    for(int i = 1; i <= N; i++) {
-        for(int j = 1; j <= N; j++) cin >> matrix[i][j];
-    }
-}
-
-void initVisited() {
-    for(int i = 0; i < 22; i++) {
-        for(int j = 0; j < 22; j++) visited[i][j] = 0;
-    }
-}
-
-void bfs() {
-    queue<pair<int, int>> q;
-    K = 1;
-    
-    while(true) {
-        int flag = 0;
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= N; j++) {
-                q.push(make_pair(i, j));
-                int money = 0;
-                initVisited();
-                visited[i][j] = 1;
-                int temp = K;
-                while(temp != 0) {
-                    int frontX = q.front().first;
-                    int frontY = q.front().second;
-                    q.pop();
-                    for(int k = 0; k < 4; k++) {
-                        if(visited[frontX + dx[k]][frontY + dy[k]] == 0) {
-                            q.push(make_pair(frontX + dx[k], frontY + dy[k]));
-                            visited[frontX + dx[k]][frontY + dy[k]] = visited[frontX][frontY] + 1;
-                            if(matrix[frontX + dx[k]][frontY + dy[k]] == 1) money += M;
-                        }
-                    }
-                    if((K + 1) * (K + 1) + K * K <= money) flag = 1;
-                    if(flag) break;
-                    temp--;
-                }
-                if(flag) break;
-            }
-            if(flag) break;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            int temp;
+            cin >> temp;
+            matrix[i][j] = temp;
+            if(temp == 1) house.push_back(make_pair(i, j));
         }
-        if(!flag) break;
-        K++;
-    }1
-    cout << K << endl;
+    }
+}
+
+void distanceCheck() {
+    int range = N + 1;
+    while(true) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                int houseCount = 0;
+                for(int k = 0; k < house.size(); k++) {
+                    if(abs(house[k].first - i) + abs(house[k].second - j) < range) houseCount++;
+                }
+                if(pow(range, 2) + pow(range - 1, 2) <= M * houseCount) answer = max(answer, houseCount);
+            }
+        }
+        if(range == 1) break;
+        range--;
+    }
 }
 
 int main() {
@@ -75,10 +44,12 @@ int main() {
     int testCase;
     cin >> testCase;
     
-    for(int i = 0; i < testCase; i++) {
+    for(int i = 1; i <= testCase; i++) {
+        answer = 0;
         cin >> N >> M;
+        house.clear();
         initMatrix();
-        initVisited();
-        bfs();
+        distanceCheck();
+        cout << "#" << i << " " << answer << endl;
     }
 }
