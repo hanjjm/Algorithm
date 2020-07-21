f0bede4f05384d357093e02ecb0ad7896475f5be
hanju Kim <hanjjm1994@naver.com>
2020-07-21 21:20:16 +0900

    C++ : BinaryGap (Bitset)

--- C++/C++/g3.cpp
+++ C++/C++/g3.cpp
@@ -1,9 +1,20 @@
-//
-//  g3.cpp
-//  C++
-//
-//  Created by KimHanJu on 2020/07/20.
-//  Copyright © 2020 KimHanJu. All rights reserved.
-//
+#include <stack>
+#include <bitset>
 
-#include <stdio.h>
+int solution(int N) {
+    stack<int> st;
+    string binary = bitset<32>(N).to_string();
+    int count = 0, maxNum = 0;
+    int binaryLength = binary.length();
+    for(int i = 0; i < binaryLength; i++) {
+        if(st.empty()) {
+            if(binary[i] == '1') st.push(1);
+        } else {
+             if(binary[i] == '1') {
+                 maxNum = max(maxNum, count);
+                 count = 0;
+             } else count++;
+        }
+    }
+    return maxNum;
+}
--- C++/C++/test.cpp
+++ C++/C++/test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+void dfs() {
+    
+}
+
 int main() {
     
 }
