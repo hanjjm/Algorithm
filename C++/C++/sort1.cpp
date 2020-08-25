c247917695352f22451ba08d829e5213bbfc205b
hanju Kim <hanjjm1994@naver.com>
2020-08-25 18:43:44 +0900

    C++ : 파일명 정렬 (Sort)

--- C++/C++/sort1.cpp
+++ C++/C++/sort1.cpp
@@ -1,21 +1,85 @@
+// 처음에 head랑 number를 따로 sort하려다가 너무 길어져버렸다..합치면 훨씬 깔끔해질듯!
 #include <string>
-#include <algorithm>
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
-vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer;
-    int len = commands.size();
-    for(int i = 0; i < len; i++) {
-        int num[commands[i][1] - commands[i][0] + 1];
-        int smallLen = commands[i][1] - commands[i][0] + 1;
-        for(int j = 0; j < smallLen; j++) {
-            num[j] = array[commands[i][0] - 1 + j];
-        }
-        sort(num, num + smallLen);
-        answer.push_back(num[commands[i][2] - 1]);
+bool compare(pair<string, int> a, pair<string, int> b) {
+    string headA = "", headB = "";
+    for(int i = 0; i < a.first.length(); i++) {
+        if(a.first[i] - '0' >= 0 && a.first[i] - '0' <= 9) break;
+        string temp = "";
+        if(a.first[i] - 'A' >= 0 && a.first[i] - 'A' <= 26) {
+            temp += a.first[i] + 32;
+        } else temp += a.first[i];
+        headA += temp;
+    }
+    for(int i = 0; i < b.first.length(); i++) {
+        if(b.first[i] - '0' >= 0 && b.first[i] - '0' <= 9) break;
+        string temp = "";
+        if(b.first[i] - 'A' >= 0 && b.first[i] - 'A' <= 26) {
+            temp += b.first[i] + 32;
+        } else temp += b.first[i];
+        headB += temp;
     }
     
+    if(headA < headB) {
+        return true;
+    } else if(headA == headB) {
+        string numberA = "", numberB = "";
+    
+        string temp = "";
+        int flag = 0;
+        for(int i = 0; i < a.first.length(); i++) {
+            if(flag && (a.first[i] - '0' > 9 || a.first[i] - '0' < 0)) break;
+            if(a.first[i] - '0' >= 0 && a.first[i] - '0' <= 9) {
+                temp += a.first[i];
+                flag = 1;
+            }
+        }
+        numberA = temp;
+        
+        temp = ""; flag = 0;
+        for(int i = 0; i < b.first.length(); i++) {
+            if(flag && (b.first[i] - '0' > 9 || b.first[i] - '0' < 0)) break;
+            if(b.first[i] - '0' >= 0 && b.first[i] - '0' <= 9) {
+                temp += b.first[i];
+                flag = 1;
+            }
+        }
+        numberB = temp;
+
+        for(int i = 0; i < numberA.length(); i++) {
+            if(numberA[i] != '0') {
+                numberA = numberA.substr(i);
+                break;
+            }
+        }
+
+        for(int i = 0; i < numberB.length(); i++) {
+            if(numberB[i] != '0') {
+                numberB = numberB.substr(i);
+                break;
+            }
+        }
+        
+        int numA = stoi(numberA), numB = stoi(numberB);
+        if(numA < numB) return true;
+        else if(numA == numB) {
+            if(a.second < b.second) return true;
+            else return false;
+        }
+        else return false;
+    }
+    else return false;
+}
+
+vector<string> solution(vector<string> files) {
+    vector<string> answer;
+    vector<pair<string, int>> vec;
+    for(int i = 1; i <= files.size(); i++) vec.push_back(make_pair(files[i - 1], i));
+    sort(vec.begin(), vec.end(), compare);
+    for(int i = 0; i < files.size(); i++) answer.push_back(vec[i].first);
     return answer;
 }
